c8fcc0857e317e7118476178905e851a54bc5d18
Tidal-999 <311968397+Tidal-999@users.noreply.github.com>
2026-08-02 20:00:23 +0800

    fix: 升级Service Worker缓存版本，确保路线图更新即时生效

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'csl-travel-v1';
+var CACHE = 'csl-travel-v2';
 var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(FILES); }).then(function () { return self.skipWaiting(); }));
